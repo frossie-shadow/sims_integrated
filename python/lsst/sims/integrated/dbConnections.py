afb47dc5de0f14928915287df8023c052c22be4c
danielsf <scott.f.daniel@gmail.com>
2016-10-11 16:05:26 -0700

    include variability in PhoSim

--- python/lsst/sims/integrated/dbConnections.py
+++ python/lsst/sims/integrated/dbConnections.py
@@ -2,13 +2,20 @@ from lsst.sims.catUtils.baseCatalogModels import (StarObj,
                                                   GalaxyBulgeObj, GalaxyDiskObj,
                                                   GalaxyAgnObj)
 
+from lsst.sims.catUtils.mixins import VariabilityStars
 from lsst.sims.catUtils.exampleCatalogDefinitions import (PhoSimCatalogPoint,
                                                           PhoSimCatalogSersic2D,
                                                           PhoSimCatalogZPoint)
 
 __all__ = ["celestial_db_dict"]
 
-celestial_db_dict = {'stars': ([StarObj], [PhoSimCatalogPoint]),
+class VariablePhoSimCatalogPoint(VariabilityStars, PhoSimCatalogPoint):
+    pass
+
+class VariablePhoSimCatalogZPoint(VariabilityStars, PhoSimCatalogZPoint):
+    pass
+
+celestial_db_dict = {'stars': ([StarObj], [VariablePhoSimCatalogPoint]),
                      'galaxies': ([GalaxyBulgeObj, GalaxyDiskObj],
                                   [PhoSimCatalogSersic2D, PhoSimCatalogSersic2D]),
-                     'agn': ([GalaxyAgnObj], [PhoSimCatalogZPoint])}
+                     'agn': ([GalaxyAgnObj], [VariablePhoSimCatalogZPoint])}
